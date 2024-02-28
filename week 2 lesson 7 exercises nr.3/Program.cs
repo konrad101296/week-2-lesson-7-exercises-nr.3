cd402b1579137cede854a74e13eb2c8d08b8a4ef
konrad101296 <konradslimak@o2.pl>
2024-02-28 19:30:18 +0100

    Add project files.

---
+++ week 2 lesson 7 exercises nr.3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter number: ");
+double number = double.Parse(Console.ReadLine());
+
+if (number < 0)
+{
+    Console.WriteLine($"{number} jest liczba ujemna");
+}
+else if (number > 0)
+{
+    Console.WriteLine($"{number} jest liczba dodatnia");
+}
+else
+{
+    Console.WriteLine("podana liczba wynosi zero");
+}
